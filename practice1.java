fca84539ea85e91e153e040cccb25cd91ad4ca2c
arjun07212003 <117085365+arjun07212003@users.noreply.github.com>
2023-01-26 13:15:38 +0530

    Add files via upload

---
+++ practice1.java
@@ -0,0 +1,11 @@
+public class practice1 {
+    public static void main(String[] args) {
+        System.out.println("The sum of these numbers is : ");
+        int num1=5;
+        int num2=8;
+        int num3=9;
+        int num4=7;
+        int sum= num1+num2+num3+num4;
+        System.out.println(sum);
+    }
+}
