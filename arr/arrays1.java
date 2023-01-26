3d08e517426defe9065f1289d83e816b503ec58a
arjun07212003 <117085365+arjun07212003@users.noreply.github.com>
2023-01-26 13:30:07 +0530

    Add files via upload

---
+++ arr/arrays1.java
@@ -0,0 +1,37 @@
+package arrays;
+
+public class arrays1 {
+
+    public static void main(String[] args) {
+
+
+       // int[] marks = {10 , 20 , 7 , 49, 39};
+        // System.out.println(marks[3]);
+
+
+        // creating  floating point array
+//        float[] marks = {10.9f, 20.5f, 7.2f, 49.0f, 39.6f};
+//        System.out.println(marks.length);
+//        System.out.println(marks[2]);
+
+//   To Find the length of the array
+       // System.out.println(marks.length);
+
+        // Array of a string;
+//        String [] name = {"Arjun","jas","garvit","rahul","saby"};
+//        System.out.println(name[3]);
+        // Printing an array using forloop.
+        int[] marks = {10 , 20 , 7 , 49, 39};
+        for (int i = 0; i<marks.length; i++)
+        {
+            System.out.println(marks[i]);
+
+
+        }
+
+        String name[] = { "jas","kap","gar","ans","arj"};
+        System.out.println(name[0]);
+
+    }
+
+}
