6ccc9144b670256452f7c386ffcf523d1058023a
Arjun Lande <117085365+arjunlande21@users.noreply.github.com>
2023-01-26 17:15:07 +0530

    Add files via upload

---
+++ practice codes/greedyAlgorithm.java
@@ -0,0 +1,57 @@
+package Interview_problems;//package Interview_problems;
+
+import java.util.*;
+
+class GFG {
+
+    static int maxSum(int arr[], int n) {
+        // Sort the array
+        Arrays.sort(arr);
+
+        // Finding the sum of arr[i]*i
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += (arr[i] * i);
+
+        return sum;
+    }
+
+    // Driven Program
+    public static void main(String[] args) {
+        int arr[] = {3, 5, 6, 1};
+        int n = arr.length;
+
+        System.out.println(maxSum(arr, n));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
